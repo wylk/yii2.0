2f1928022e30617ef4d66ed097848697f3325f0f
jioajiao520 <760553350@qq.com>
2017-08-24 16:48:58 +0800

    用户管理及角色管理

--- web/modules/admin/Admin.php
+++ web/modules/admin/Admin.php
@@ -29,4 +29,39 @@ class Admin extends \yii\base\Module
 
         // custom initialization code goes here
     }
+     protected function success($message='',$jumpUrl='',$ajax=false) {
+        $this->dispatchJump($message,1,$jumpUrl,$ajax);
+    }
+     private function dispatchJump($message,$status=1,$jumpUrl='',$ajax=false) {
+        $jumpUrl = !empty($jumpUrl)? (is_array($jumpUrl)?Url::toRoute($jumpUrl):$jumpUrl):'';
+        if(true === $ajax || Yii::$app->request->isAjax) {// AJAX提交
+            $data           =   is_array($ajax)?$ajax:array();
+            $data['info']   =   $message;
+            $data['status'] =   $status;
+            $data['url']    =   $jumpUrl;
+            $this->ajaxReturn($data);
+        }
+        // 成功操作后默认停留1秒
+        $waitSecond = 3;
+
+        if($status) { //发送成功信息
+            $message = $message ? $message : '提交成功' ;// 提示信息
+            // 默认操作成功自动返回操作前页面
+            echo $this->renderFile(Yii::$app->params['action_success'],[
+                'message' => $message,
+                'waitSecond' => $waitSecond,
+                'jumpUrl' => $jumpUrl,
+            ]);
+        }else{
+            $message = $message ? $message : '发生错误了' ;// 提示信息
+            // 默认发生错误的话自动返回上页
+            $jumpUrl = "javascript:history.back(-1);";
+            echo $this->renderFile(Yii::$app->params['action_error'], [
+                'message' => $message,
+                'waitSecond' => $waitSecond,
+                'jumpUrl' => $jumpUrl,
+            ]);
+        }
+        exit;
+    }
 }
